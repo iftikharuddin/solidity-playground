24d2ab5f37e2cb9f798fa28ff828c1e0b6ccafce
Iftikhar uddin <iftikhar@connextar.com>
2022-01-10 19:14:15 +0500

    Practicing Solidity units

---
+++ MyUnits.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.11;
+
+contract MyUnits {
+
+    // assert(1 wei == 1);
+    // assert(1 gwei == 1e9);
+    // assert(1 ether == 1e18);
+
+    // function f(uint start, uint daysAfter) public {
+    //     if (block.timestamp >= start + daysAfter * 1 days) {
+    //     // ...
+    //     }
+    // }
+
+    // 1 == 1 seconds
+
+    // 1 minutes == 60 seconds
+
+    // 1 hours == 60 minutes
+
+    // 1 days == 24 hours
+
+    // 1 weeks == 7 days
+
+}
