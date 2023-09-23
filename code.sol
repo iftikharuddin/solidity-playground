9250c613b2110cd4e4ba80dc561b060f8df6d759
Iftikhar uddin <iftikhar@connextar.com>
2023-09-23 23:47:42 +0500

    well solidity by who?

--- code.sol
+++ code.sol
@@ -5,3 +5,7 @@ pragma solidity ^0.8.11;
 /// @title Playing with SOLIDITY
 /// @author Iftikhar
 /// @dev Iftikhar
+/// Gaving Wood: Solidity was designed by Gavin Wood and developed by Christian Reitwiessner, Alex Beregszaszi,
+/// and several former Ethereum core contributors. Programs in Solidity run on Ethereum Virtual Machine or on compatible virtual machines.
+
+
