07c4e2384d6e646756080031e9bc25f0d6f5ffdb
Iftikhar uddin <iftikhar@connextar.com>
2023-09-24 15:04:15 +0500

    Added more notes

---
+++ fundamentals.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/// pragma solidity ^0.8.11;
+
+/// This doc will contain all the research and my notes which was taken during learning of blockchain
+
+/// @title Playing with SOLIDITY
+/// @author Iftikhar
+/// @dev Iftikhar
+/// Gaving Wood: Solidity was designed by Gavin Wood and developed by Christian Reitwiessner, Alex Beregszaszi,
+/// and several former Ethereum core contributors. Programs in Solidity run on Ethereum Virtual Machine or on compatible virtual machines.
+/// First appeared: August 2014, Implementation language: C++
+/// Gensis Block: It's the first block in a blockchain
+/// As of september 15, 2022 ETH is proof of work
+/// PoW uses a lot of energy
+/// PoW we call it miners and in PoS we call it validators
+/// Layer 1: Base leyer blockchain implementation ( BTC, ETH, AVAX etc )
+/// Layer 2: Any Application built on top of layer 1 ( Chailink, ARBITRIUM, OP )
+/// Rollups derive their security from base layer
+/// Side chains derive their security from their own protocols
+/// Sharding and rollups are scalability solutions on layer 1
+
+
