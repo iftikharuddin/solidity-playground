9fb4c0ce8b55bea73a03ecd5b3ecd4169399e62f
Iftikhar uddin <iftikhar@connextar.com>
2023-09-24 23:39:18 +0500

    a simple contract to store favorite number and get it

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,7 +2,19 @@
 
 pragma solidity ^0.8.19; // version
 
-// Contract Opening
+// Contract Simple Storage
 contract SimpleStorage {
+    // if you don't specifiy visibility specifier the default will be internal
+    uint256 public favoriteNumber; // deafult value for uint256 is zero
 
+    function setFavoriteNumber(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    // View, pure functions don’t cost gas but if you call it in a setter
+    // function which cost gas, then it will add gas cost
+    // view, pure
+    function getFN() public view returns(uint256) {
+        return favoriteNumber;
+    }
 }
