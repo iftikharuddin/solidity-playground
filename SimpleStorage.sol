4e9ea5c0cdc2b8c467c9155f4617b78b2055f142
Iftikhar uddin <iftikhar@connextar.com>
2023-09-25 13:20:01 +0500

    just practiced with more data types and learning

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -7,6 +7,18 @@ contract SimpleStorage {
     // if you don't specifiy visibility specifier the default will be internal
     uint256 public favoriteNumber; // deafult value for uint256 is zero
 
+    // let's define a custom var
+    struct Person {
+        string name;
+        uint256 fNumber;
+    }
+
+    // dynamic array
+    Person[] public listOfPeople;
+
+    // let's play with mappings
+    mapping (string => uint256) public nameToFavoriteNumber;
+
     function setFavoriteNumber(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
@@ -16,5 +28,12 @@ contract SimpleStorage {
     // view, pure
     function getFN() public view returns(uint256) {
         return favoriteNumber;
+    }
+
+    // add person name and favorite number to Person Array
+    // Calldata can’t be modified while memory can be modified, both are temp storing vars
+    function addPerson(string memory _name, uint256 _fNumber) public {
+        listOfPeople.push(Person(_name, _fNumber));
+        nameToFavoriteNumber[_name] = _fNumber;
     }
 }
